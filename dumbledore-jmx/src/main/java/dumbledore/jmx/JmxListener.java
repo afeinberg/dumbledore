3fb80975bd7877788ab4edd2ca7f2f20768fbdab
Alex Feinberg <alex@strlen.net>
2011-08-04 01:22:23 -0700

    Skeleton for dumbledore-jmx

---
+++ dumbledore-jmx/src/main/java/dumbledore/jmx/JmxListener.java
@@ -0,0 +1,19 @@
+package dumbledore.jmx;
+
+import dumbledore.metrics.MetricWrapper;
+import dumbledore.metrics.MetricsListener;
+
+/**
+ * TODO: implement this
+ */
+public class JmxListener implements MetricsListener {
+
+    public void added(String domain, String type, MetricWrapper metric) {
+
+    }
+
+
+    public void removed(String domain, String type) {
+
+    }
+}
