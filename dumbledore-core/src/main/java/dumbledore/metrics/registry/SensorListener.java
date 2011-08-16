61757bb193094f5dd9b29b42ed7451773d65b38e
Alex Feinberg <alex@strlen.net>
2011-08-15 17:36:34 -0700

    Commit minor changes

--- dumbledore-core/src/main/java/dumbledore/metrics/registry/SensorListener.java
+++ dumbledore-core/src/main/java/dumbledore/metrics/registry/SensorListener.java
@@ -14,7 +14,7 @@ public interface SensorListener {
      * Executed after a sensor is added
      * @param domain Sensor's domain, e.g., "voldemort.store.stats"
      * @param type Sensor's type, e.g., "aggregate-statistics"
-     * @param sensor A {@link SensorWrapper} instance wrapping the sensor object
+     * @param sensor A {@link SensorDescriptor} instance wrapping the sensor object
      */
     public void registered(String domain, String type, SensorDescriptor sensor);
 
