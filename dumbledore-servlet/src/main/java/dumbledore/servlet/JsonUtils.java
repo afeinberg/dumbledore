061076648eace9fb2ffb84f055f7bf4c2d39b6e3
Alex Feinberg <alex@strlen.net>
2011-08-15 18:14:56 -0700

    Fix JsonUtils

--- dumbledore-servlet/src/main/java/dumbledore/servlet/JsonUtils.java
+++ dumbledore-servlet/src/main/java/dumbledore/servlet/JsonUtils.java
@@ -1,1 +1,50 @@
-fatal: pathspec 'dumbledore-servlet/src/main/java/dumbledore/servlet/JSONUtils.java' did not match any files
+package dumbledore.servlet;
+
+import dumbledore.metrics.AttributeDescriptor;
+import dumbledore.metrics.SensorDescriptor;
+
+/**
+ *
+ */
+public class JsonUtils {
+
+    public static void sensorToJson(SensorDescriptor sensor, StringBuilder sb)  {
+        sb.append("{\n");
+        sb.append("\"name\": \"");
+        sb.append(sensor.getName());
+        sb.append("\", ");
+        sb.append("\n\"description\": \"");
+        sb.append(sensor.getDescription());
+        sb.append("\", ");
+        sb.append("\n\"attributes\": {\n");
+
+        int i = 0;
+        for(AttributeDescriptor attribute: sensor.getAttributes()) {
+            if(i++ > 0)
+                sb.append(",\n");
+            attributeToJson(sensor, attribute, sb);
+        }
+        sb.append("\n}");
+    }
+
+    public static void attributeToJson(SensorDescriptor sensor,
+                                       AttributeDescriptor attribute,
+                                       StringBuilder sb) {
+        String name = attribute.getName();
+        sb.append("  \"");
+        sb.append(name);
+        sb.append("\": {");
+        sb.append("\n    \"description\": \"");
+        sb.append(attribute.getDescription());
+        sb.append("\", ");
+        sb.append("\n    \"data_type\": \"");
+        sb.append(attribute.getDataType());
+        sb.append("\", ");
+        sb.append("\n    \"metric_type\": \"");
+        sb.append(attribute.getMetricType());
+        sb.append("\", ");
+        sb.append("\n    \"value\": \"");
+        sb.append(sensor.getAttributeValue(name));
+        sb.append("\"\n  }");
+    }
+}
